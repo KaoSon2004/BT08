eac49ec63d2254fe066b1585ccf286d378da0fad
KaoSon2004 <78968595+KaoSon2004@users.noreply.github.com>
2021-03-17 13:25:11 +0700

    Update BT08-A-4.cpp

--- BT08-A-4.cpp
+++ BT08-A-4.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()  {
    char **s=new char *;// cấp phát vùng nhớ cho con trỏ
    char foo[] = "Hello World";
-   *s = foo;
-   printf("s is %s\n",*s);//sửa s[0] thành *s[0]
-   *s[0] = foo;
+   *s = foo; //*s truy nhập vùng nhớ không xác định
+   printf("s is %s\n",s);
+   s[0] = foo;// lỗi s chưa khởi tạo
    printf("s[0] is %s\n",s[0]);
    return(0);
 }
